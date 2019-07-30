00bc47b5690dc346ab89185d106cd3f717feee02
Bankole <techybanky@gmail.com>
2019-07-30 22:03:51 +0100

    added Webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const path = require('path');
-const open = require('open');
+import express from 'express';
+import { join } from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 const port = 5000;
 const app = express();
+const compiler = webpack(config);
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'))
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}))
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, '../src/index.html'))
 });
 
-app.listen(port, function(err) {
+app.listen(port, (err) => {
   err ? console.log(err): open(`http://localhost:${port}`);
 });
