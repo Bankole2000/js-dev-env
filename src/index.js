091200185ece9809fa67c3294fb7659fe2d65fa6
Bankole <techybanky@gmail.com>
2019-07-31 05:06:49 +0100

    Added ESLint and lint watch

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
-debugger;
-console.log(`I will pay ${courseValue} for this awesome Course!`);
+console.log(`I will pay ${courseValue} for this awesome Course!`); // eslint-disable-line no-console
 
 
